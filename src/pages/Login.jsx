9eb94eead2a5fe3bf344c50af25e50797f556a2d
Salah Mel-Catherine Atuma <melcatherinesalah@gmail.com>
2025-09-26 11:08:54 +0100

    setting up routes

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style.css';
+
+const Login = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h2>Login Page</h2>
+      {/* Add your login form here */}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style.css';
+
+const Register = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h2>Registration Page</h2>
+      {/* Add your registration form here */}
+    </div>
+  );
+};
+
+export default Register;
